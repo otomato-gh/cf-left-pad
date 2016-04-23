ddfaa060443866d4b6d892180733213ded391c82
Ant Weiss <ant.weiss@gmail.com>
2016-04-23 19:15:33 +0300

    add version printout

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 module.exports = leftpad;
+var pjson = require('./package.json');
 
 function leftpad (str, len, ch) {
+  console.log(pjson.name+"-"+pjson.version);
   str = String(str);
-
+  
   var i = -1;
 
   if (!ch && ch !== 0) ch = ' ';
